6d7a995656cdc10e16aefd9ceb83673de4cd5a71
VincentGonnet <vincent.gonnet08@gmail.com>
2023-09-30 00:58:04 +0200

    refractor: removed inputpromptmanager - not needed

--- Assets/Scripts/Behaviours/UI/InputPromptController.cs
+++ Assets/Scripts/Behaviours/UI/InputPromptController.cs
@@ -17,8 +17,35 @@ public class InputPromptController : MonoBehaviour
     [SerializeField] private float size = 1f;
 
     public void Start() {
-        gameObject.GetComponent<SpriteRenderer>().sprite = InputPromptManager.Instance.getSpriteFromIndex(controller, choiceIndex);
+        gameObject.GetComponent<SpriteRenderer>().sprite = getSpriteFromIndex(controller, choiceIndex);
         gameObject.transform.localScale = new Vector3(size, size, size);
     } 
+
+    private Sprite getSpriteFromIndex(Controller controller, int index)
+    {
+        string spritePath = "InputPrompts/";
+
+        switch (controller)
+        {
+            case Controller.KeyboardAndMouse:
+                spritePath += "KeyboardAndMouse";
+                break;
+            case Controller.PS4:
+                spritePath += "PS4";
+                break;
+            case Controller.Xbox:
+                spritePath += "Xbox";
+                break;
+            case Controller.Switch:
+                spritePath += "Switch";
+                break;
+            default:
+                break;
+        }
+
+        Sprite[] sprites = Resources.LoadAll<Sprite>(spritePath);
+
+        return sprites[index];
+    }
 
 }
--- Assets/Scripts/Managers/InputPromptManager.cs
+++ Assets/Scripts/Managers/InputPromptManager.cs
@@ -10,31 +10,4 @@ public class InputPromptManager : Singleton<InputPromptManager>
         DontDestroyOnLoad(this.gameObject);
     }
 
-    public Sprite getSpriteFromIndex(Controller controller, int index)
-    {
-        string spritePath = "InputPrompts/";
-
-        switch (controller)
-        {
-            case Controller.KeyboardAndMouse:
-                spritePath += "KeyboardAndMouse";
-                break;
-            case Controller.PS4:
-                spritePath += "PS4";
-                break;
-            case Controller.Xbox:
-                spritePath += "Xbox";
-                break;
-            case Controller.Switch:
-                spritePath += "Switch";
-                break;
-            default:
-                break;
-        }
-
-        Sprite[] sprites = Resources.LoadAll<Sprite>(spritePath);
-
-        return sprites[index];
-    }
-
 }
