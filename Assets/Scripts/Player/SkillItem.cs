6fb5db08894769dbb98653befe0b4d206775ad58
AlbanSdl <alban.delavoreille@free.fr>
2023-10-01 12:18:29 +0200

    animate skill items

--- Assets/Scripts/Player/SkillItem.cs
+++ Assets/Scripts/Player/SkillItem.cs
@@ -10,7 +10,7 @@ public class SkillItem : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player") {
             other.GetComponent<PlayerProperties>().PushToBackpack(skill);
-            Destroy(gameObject);
+            Destroy(transform.parent.gameObject);
         }
     }
 }
